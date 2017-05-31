6624062055e7929f91c2190e14310daa2d6949bc
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2017-05-31 18:16:56 +0100

    Get data from checkpoints from backend and display them

--- aurelia-app/src/app.js
+++ aurelia-app/src/app.js
@@ -1,25 +1,33 @@
-import {Checkpoint} from './checkpoint';
+// import {io} from 'socket.io-client';
+// var socket = io.connect('http://localhost:9000');
+//
+// export class App {
+//   activate() {
+//     socket.on('checkpoints',   // <- only works once (when loading the page) but doesn't listen after
+//       function (checkpoints) {
+//         this.checkpoints = checkpoints;
+//       }.bind(this));
+//   }
+//
+//   constructor() {
+//     this.heading = "Checkpoints";
+//     this.checkpoints = [];
+//   }
+// }
+import {HttpClient} from 'aurelia-http-client';
 
 export class App {
+
   constructor() {
     this.heading = "Checkpoints";
-    this.checkpoints = [];
-    this.heartRate = 0;
-    this.calories = 0;
-    this.distance = 0;
-  }
+    let client = new HttpClient()
+      .configure(x => {
+        x.withBaseUrl('http://localhost:9000');
+      });
 
-  addCheckpoint() {
-    this.checkpoints.push(new Checkpoint(this.heartRate, this.calories, this.distance));
-    this.heartRate = 0;
-    this.calories = 0;
-    this.distance = 0;
-  }
-
-  removeCheckpoint(checkpoint) {
-    let index = this.checkpoints.indexOf(checkpoint);
-    if (index !== -1) {
-      this.checkpoints.splice(index, 1);
-    }
+    client.get('checkpoints').then(function (response) {
+      var checkpoints = JSON.parse(response.response);
+      this.checkpoints = checkpoints;
+    }.bind(this));
   }
 }
