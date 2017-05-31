a97dfb9370f6dbe71e80047ababdb69bb5abdf8d
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2017-05-31 23:04:45 +0100

    Implement sockets for frontend

--- aurelia-app/src/app.js
+++ aurelia-app/src/app.js
@@ -1,38 +1,32 @@
-// import {io} from 'socket.io-client';
-// var socket = io.connect('http://localhost:9000');
-//
-// export class App {
-//   activate() {
-//     socket.on('checkpoints',   // <- only works once (when loading the page) but doesn't listen after
-//       function (checkpoints) {
-//         this.checkpoints = checkpoints;
-//       }.bind(this));
-//   }
-//
-//   constructor() {
-//     this.heading = "Checkpoints";
-//     this.checkpoints = [];
-//   }
-// }
 import {HttpClient} from 'aurelia-http-client';
 import Dygraph from '../node_modules/dygraphs/dist/dygraph';
+import io from '../node_modules/socket.io-client/dist/socket.io';
+var socket = io.connect('http://localhost:9000');
+
 
 export class App {
 
   constructor() {
     this.heading = "Checkpoints";
+
     let client = new HttpClient()
       .configure(x => {
         x.withBaseUrl('http://localhost:9000');
       });
 
     client.get('checkpoints').then(function (response) {
-      var checkpoints = JSON.parse(response.response);
-      console.log(checkpoints)
-      console.log(new Date(checkpoints[0].createdAt));
-      console.log(new Date());
-      chartHeartRate(checkpoints)
+      this.checkpoints = JSON.parse(response.response);
+      chartHeartRate(this.checkpoints)
     }.bind(this));
+  }
+
+  activate() {
+    socket.on('checkpoint:save',   // <- only works once (when loading the page) but doesn't listen after
+      function (checkpoint) {
+        console.log("Getting socket updates")
+        this.checkpoints.push(checkpoint)
+        chartHeartRate(this.checkpoints)
+      }.bind( this ) );
   }
 }
 
