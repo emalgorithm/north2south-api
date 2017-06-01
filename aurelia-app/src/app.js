e0b55cb5acdaa83260bc339a6968838ed09234d2
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2017-06-01 17:42:56 +0100

    HeartRate, Calories, Distance real time

--- aurelia-app/src/app.js
+++ aurelia-app/src/app.js
@@ -16,38 +16,46 @@ export class App {
 
     client.get('checkpoints').then(function (response) {
       this.checkpoints = JSON.parse(response.response);
-      chartHeartRate(this.checkpoints)
+      this.heartRateChart = chartHeartRate(this.checkpoints)
+      this.calories = 0
+      this.distance = 0
+      if (this.checkpoints.length > 0) {
+        this.calories = this.checkpoints.last.calories || 0
+        this.distance = this.checkpoints.last.distance || 0
+      }
+
     }.bind(this));
   }
 
   activate() {
-    socket.on('checkpoint:save',   // <- only works once (when loading the page) but doesn't listen after
+    socket.on('checkpoint:save',
       function (checkpoint) {
-        console.log("Getting socket updates")
+        console.log("Getting socket updates:  " + checkpoint)
         this.checkpoints.push(checkpoint)
-        chartHeartRate(this.checkpoints)
+        updateChart(this.heartRateChart, this.checkpoints)
+        this.calories = checkpoint.calories
+        this.distance = checkpoint.distance
       }.bind( this ) );
   }
 }
 
 var chartHeartRate = function (checkpoints) {
   var data = checkpoints.map(c => [new Date(c.createdAt), c.heartRate])
-  console.log(data)
-  var g = new Dygraph(document.getElementById("chart"),
+  return chart("heartRate", data)
+}
+
+var chart = function (chartName, data) {
+  return new Dygraph(
+    document.getElementById(chartName),
     data,
     {
       legend: 'always',
-      xlabel: "Time",
-      labels: [ "Time", "HeartRate"],
-      //title: 'HeartRate',
-      //showRoller: true,
-      ylabel: 'Temperature (F)',
+      labels: ["Time", chartName],
+      //title: chartName,
     });
+}
 
-  // window.intervalId = setInterval(function() {
-  //   var x = new Date();  // current time
-  //   var y = Math.random() * 150;
-  //   data.push([x, y]);
-  //   g.updateOptions( { 'file': data } );
-  // }, 1000);
+var updateChart = function (chart, checkpoints) {
+  var data = checkpoints.map(c => [new Date(c.createdAt), c[chart]])
+  chart.updateOptions( { 'file': data } );
 }
