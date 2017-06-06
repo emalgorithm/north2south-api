b489ff49a2996e1e7f2956daf3a24463ae457234
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2017-06-06 16:55:07 +0100

    Implement adding markers to map

--- aurelia-app/src/map.js
+++ aurelia-app/src/map.js
@@ -12,9 +12,29 @@ exports.startMap = function() {
   document.querySelector('head').appendChild(scriptElement);
 };
 
+var labelIndex = 0;
+
 function initMap() {
+  // In the following example, markers appear when the user clicks on the map.
+
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
+  });
+
+  // This event listener calls addMarker() when the map is clicked.
+  google.maps.event.addListener(map, 'click', function(event) {
+    addMarker(event.latLng, map);
+  });
+}
+
+// Adds a marker to the map.
+function addMarker(location, map) {
+  // Add the marker at the clicked location, and add the next-available label
+  // from the array of alphabetical characters.
+  var marker = new google.maps.Marker({
+    position: location,
+    label: labelIndex++,
+    map: map
   });
 }
