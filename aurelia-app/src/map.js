802201d614708acb4805fea0f188d266275a237a
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2017-06-06 16:50:12 +0100

    Add function to window object directly instead that injecting script

--- aurelia-app/src/map.js
+++ aurelia-app/src/map.js
@@ -1,18 +1,5 @@
 exports.startMap = function() {
-  // Add script for initMap function code
-  let initMapScriptElement = document.createElement('script');
-  initMapScriptElement.textContent = `var map;
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-      });
-    }
-    `;
-  document.querySelector('head').appendChild(initMapScriptElement);
-  initMapScriptElement.onload = () => {
-    console.log("Init Map script element has been loaded");
-  };
+  window.initMap = initMap;
 
   // Add script which loads the map, and then has a callback to initMap
   let scriptElement = document.createElement('script');
@@ -24,3 +11,10 @@ exports.startMap = function() {
   };
   document.querySelector('head').appendChild(scriptElement);
 };
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
