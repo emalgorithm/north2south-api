b5e4db6c3f1f0f88ea7f2cc672b68a8d0207e1ae
Tencho Tenev <tt1215@ic.ac.uk>
2017-06-12 11:04:19 +0100

    Use api to fetch journeys

--- aurelia-app/src/services/api.js
+++ aurelia-app/src/services/api.js
@@ -2,13 +2,42 @@ import 'fetch'
 import { HttpClient } from 'aurelia-fetch-client';
 
 export class RestApi {
-	isRequesting = false
-	client = new HttpClient().configure(c => {
-        c.withBaseUrl('/')
-      })
+
+	static inject = [HttpClient]
+
+	constructor(http) {
+		this.isRequesting = false
+		this.http = http.configure(config => {
+			config
+				.withBaseUrl('/')
+				.withDefaults({
+					headers: {
+						'Accept': 'application/json',
+						'X-Requested-With': 'Fetch'
+					}
+				})
+				.withInterceptor({
+					request(request) {
+						console.log('Reuqesting ${request.method} ${request.url}')
+						return request
+					},
+
+					response(response) {
+						console.log(`Received ${response.status} ${response.url}`);
+						return response;
+					}
+
+				})
+		})
+	}
 
 	getJourneyList() {
 		this.isRequesting = true
-		return 
+		return this.http.fetch('journeys')
+			.then(response => response.json())
+			.then(journeys => {
+				console.log(journeys)
+				return journeys
+			})
 	}
 }
