ae98f3aa7ea76d958f532df1343cbcd4f1989d48
Tencho Tenev <tt1215@ic.ac.uk>
2017-06-12 09:54:47 +0100

    Move journey data to view model

--- aurelia-app/src/components/journey/journey-list.js
+++ aurelia-app/src/components/journey/journey-list.js
@@ -1,4 +1,27 @@
 export class JourneyList {
-  constructor() {
-  }
+    constructor() {
+        var journeyViewModel = {
+            name: 'North By South 2017',
+            owner: {
+                name: 'James Redden',
+                avatar: '../assets/img/norway-me.jpg',
+                profileUrl: '#'
+            },
+            focusedMessage: {
+                title: 'Reflecting on day 21',
+                content: 'In 2017 I will attempt a full distance, solo, unsupported and unaided ski to the South Pole. The aim of this expedition is to raise money for Soundabout.org.uk, a UK-based charity which uses music as a form of therapy for disabled childrend and adults. I will also be raising money for a second charity and the official announcement will be made soon. Start date will be November 2017 (lots of planning and funding to raise hence announcing now; 17 months before I set off). Distance is about 683 miles and one I’m aiming to complete in under 30 days. In between...',
+            },
+            focusedCheckpoint: {
+                icon: 'directions_run',
+                title: 'Distance covered today',
+                content: '24.245 km'
+            }
+        }
+
+        this.journeys = [
+        	journeyViewModel,
+        	journeyViewModel,
+        	journeyViewModel
+        ]
+    }
 }
