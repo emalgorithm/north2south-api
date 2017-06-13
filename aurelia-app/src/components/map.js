7bcb9a54457d77937e2811c87601ded20c208b0c
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2017-06-13 13:12:58 +0100

    Synchronize map initialization with loading of its script

--- aurelia-app/src/components/map.js
+++ aurelia-app/src/components/map.js
@@ -13,9 +13,7 @@ export class Map {
 
   activate() {
     console.log("Inside Map attached() method");
-    window.initMap = this.initMap();
-
-    this.addMapScript()
+    this.addMapScript(this.initMap())
   }
 
   initMap() {
@@ -25,19 +23,18 @@ export class Map {
         center: {lat: -34.397, lng: 150.644},
         zoom: 2
       });
-      console.log("Inside initMap, GoogleMap is: ");
-      console.log(this);
     }.bind(this));
   }
 
-  addMapScript() {
+  addMapScript(callback) {
     // Add script which loads the map, and then has a callback to initMap
     let scriptElement = document.createElement('script');
-    scriptElement.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAXwYqFZxpik8C0iIJgwuTroW1KyCSX_jk&callback=initMap";
+    scriptElement.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAXwYqFZxpik8C0iIJgwuTroW1KyCSX_jk";
     scriptElement.async = true;
     scriptElement.defer = true;
     scriptElement.onload = () => {
       console.log("Google maps script element has been loaded");
+      callback();
       this.eventAggregator.publish("mapLoaded", this);
     };
     document.querySelector('head').appendChild(scriptElement);
@@ -49,8 +46,6 @@ export class Map {
     }
 
     console.log("Adding a marker at latitude: " + latitude + " and longitude: " + longitude);
-    console.log("GoogleMap is: ");
-    console.log(this.googleMap);
 
     var location = new google.maps.LatLng(latitude, longitude);
 
