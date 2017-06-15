848bafb6efb187f22beb553dab55cd6fff948aee
panpan2 <p.panayiotou2@gmail.com>
2017-06-15 16:00:46 +0100

    Query user data and display followers in profile

--- aurelia-app/src/components/profile.js
+++ aurelia-app/src/components/profile.js
@@ -1,8 +1,17 @@
-export class Profile {
-  constructor() {
+import { RestApi } from 'services/api'
 
+export class Profile {
+
+  static inject = [RestApi]
+
+  constructor(restApi) {
+    this.restApi = restApi
   }
+
   activate(params, routeConfig) {
     console.log('the parameter is' + params.id)
+    return this.restApi.getUser(params.id).then(user => {
+      Object.assign(this, ...user);
+    })
   }
 }
