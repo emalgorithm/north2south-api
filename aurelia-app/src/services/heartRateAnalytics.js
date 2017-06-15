e429f6df8a1dc8e00c95b7235ed25fa85cad1065
Tencho Tenev <tt1215@ic.ac.uk>
2017-06-15 23:40:02 +0100

    Apply value function on heart rate for analysis

--- aurelia-app/src/services/heartRateAnalytics.js
+++ aurelia-app/src/services/heartRateAnalytics.js
@@ -10,8 +10,8 @@ export class HeartRateAnalyticsService {
   analysis() {
     this.weeklyCheckpoints = DataAnalyticsHelpers.groupCheckpointsByWeek(this.checkpoints)
 
-    let thisWeekValue = this.valueFunction(this.weeklyCheckpoints.thisWeek)
-    let beforeValue = this.valueFunction(this.weeklyCheckpoints.before)
+    let thisWeekValue = this.valueFunction(this.weeklyCheckpoints.thisWeek.map(c => c.heartRate))
+    let beforeValue = this.valueFunction(this.weeklyCheckpoints.before.map(c => c.heartRate))
 
     return DataAnalyticsHelpers.analyse(thisWeekValue, beforeValue,
       'recovery than previous weeks', ['faster', 'slower'])
